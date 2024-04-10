63e77cfdb9b8a8e33b3e42c54bc89361a83f9d73
Ronny Endara <rojaence@gmail.com>
2024-04-10 18:20:23 -0500

    UPDATE carouselComponent, simplify scrolling logic

--- src/app/modules/media-carousel/components/carousel/carousel.component.ts
+++ src/app/modules/media-carousel/components/carousel/carousel.component.ts
@@ -1,23 +1,25 @@
-import { Component, ElementRef, ViewChild, OnDestroy, ContentChildren, QueryList, OnChanges, SimpleChanges, OnInit, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnDestroy, ContentChildren, QueryList, AfterViewInit, Input } from '@angular/core';
 import { CarouselItemComponent } from '../carousel-item/carousel-item.component';
 import { Subscription } from 'rxjs';
-import { fadeAnimation } from '../../../../components/animations/fade-animation';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
-  animations: [fadeAnimation]
 })
 
 export class CarouselComponent implements OnDestroy, AfterViewInit {
   scrollX = 0;
   scrollEnd = false;
+  @Input() disabled = false;
   @ViewChild('slideList') slideList: ElementRef<HTMLDivElement> | undefined;
   @ViewChild('leftButton') leftButton: ElementRef | undefined;
   @ViewChild('rightButton') rightButton: ElementRef | undefined;
   @ContentChildren(CarouselItemComponent) items = new QueryList<CarouselItemComponent>();
   itemsChangeSubscription: Subscription | undefined;
+  itemsToShow: CarouselItemComponent[] = [];
+
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.slideList?.nativeElement.addEventListener('scroll', this.onScrollEvent);
@@ -50,43 +52,20 @@ export class CarouselComponent implements OnDestroy, AfterViewInit {
   scrollShift(direction: 'right' | 'left') {
     const listElement = this.slideList!.nativeElement;
     const listRects = listElement.getBoundingClientRect();
-    const listChildren = Array.from(listElement.children) as HTMLElement[];
-    let reference = this.getReferenceChild(direction, listChildren);
+
     let value = 0;
     if (direction === 'right') {
       if (this.scrollEnd) return;
-      value = reference ? reference.getBoundingClientRect().x - listRects.x : 0;
+      value = listRects.width;
     }
     if (direction === 'left') {
       if (this.scrollX === 0) return;
-      value -= reference ? listRects.width - reference.getBoundingClientRect().right : 0
+      value -= listRects.width;
     }
-    this.scrollCommit(value);
+    this.scrollList(value);
   }
 
-  getReferenceChild(direction: 'right' | 'left', children: HTMLElement[]): HTMLElement | undefined {
-    let reference: HTMLElement | undefined;
-    const listRects = this.slideList!.nativeElement.getBoundingClientRect();
-
-    for (let i = 0; i < children.length; i++) {
-      let rect = children[i].getBoundingClientRect();
-      if (direction === 'right') {
-        if (rect.right > listRects.right && rect.left < listRects.right) {
-          reference = children[i];
-          break;
-        }
-      }
-      if (direction === 'left') {
-        if (rect.left >= listRects.left) {
-          reference = children[i - 1];
-          break;
-        }
-      }
-    }
-    return reference;
-  }
-
-  scrollCommit(value: number) {
+  scrollList(value: number) {
     this.slideList!.nativeElement.scrollLeft += value;
     this.scrollX += value;
     this.endScrollCheck();
